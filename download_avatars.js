6e50d26fadb51b9eda24f4e4432b3b3473fb384b
Julia <juliaromanowski@gmail.com>
2019-01-15 00:23:35 +0000

    implemented download images by url with hard cording url string

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,41 @@
 var request = require('request');
 var secrets = require('./secrets');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
-
 function getRepoContributors(repoOwner, repoName, cb) {
     var options = {
         url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
         headers: {
-            'User-Agent': 'request'
+            'User-Agent': 'request',
+            'Authorization': 'token ' + secrets.GITHUB_TOKEN
         }
     };
 
     request(options, function (err, res, body) {
-        cb(err, body);
+        if (!err && res.statusCode == 200) {
+            var contributors = JSON.parse(body);
+            cb(err, contributors);
+        };
     });
 }
 
-getRepoContributors("jquery", "jquery", function (err, result) {
-    console.log("Errors:", err);
-    console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function (err, contributors) {
+    contributors.forEach(function (contributor) {
+    });
 });
+
+function downloadImageByURL(url, filePath) {
+    var fs = require('fs');
+    request.get(url)
+        .on('error', function (err) {
+            throw err;
+        })
+        .on('response', function (response) {
+            console.log('Response Status Code: ', response.statusCode);
+            console.log('Response Message: ', response.statusMessage);
+            console.log('Content Type: ', response.headers['content-type']);
+            console.log('Download complete')
+        })
+        .pipe(fs.createWriteStream(filePath))
+}
+
+downloadImageByURL("https://avatars2.githubusercontent.com/u/2741?v=3&s=466", "avatars/kvirani.jpg")
